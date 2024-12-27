605d46e1ac6b0d92b69cbecff5ab36c51830b023
Ciaralooney <66528901+Ciaralooney@users.noreply.github.com>
2024-12-27 20:44:24 +0000

    Update Putting on the Ritz lyrics
    added more lyrics and fixed an issue where ohbot did not have enough time to say a line

--- Showtape 1 - Putting on the Ritz/Putting on the Ritz v0.1.py
+++ Showtape 1 - Putting on the Ritz/Putting on the Ritz v0.1.py
@@ -1,5 +1,4 @@
 from ohbot import ohbot
-import time
 
 ohbot.reset()
 
@@ -9,7 +8,6 @@ ohbot.wait(0.4)
 ohbot.move(ohbot.LIDBLINK, 10)
 
 
-# 1st verse
 # 2nd parameter (False) means ohbot will continue moving while talking
 # 3rd parameter (True) means ohbot will lipsync to the text
 ohbot.say("If you're blue, and you don't know where to go to", False, True)
@@ -22,25 +20,47 @@ ohbot.move(ohbot.EYETURN, 0)
 ohbot.move(ohbot.LIDBLINK, 0)
 ohbot.wait(0.4)
 ohbot.move(ohbot.LIDBLINK, 10)
-
+ohbot.wait(2)
 ohbot.say("Why don't you go where fashion sits?", False, True)
 
 # Moving the eyes
 ohbot.move(ohbot.EYETURN, 7)
 ohbot.move(ohbot.EYETILT, 7)
-time.sleep(.5)  # Wait for .5 seconds
+ohbot.wait(0.5)  # Wait for .5 seconds
 ohbot.move(ohbot.HEADTURN, 7, spd=1)  # moving the head to the random number at speed 1 (slow)
 ohbot.move(ohbot.HEADNOD, 7, spd=1)
-time.sleep(.5)  # Wait for .5 seconds
+ohbot.wait(0.5)  # Wait for .5 seconds
 ohbot.move(ohbot.EYETURN, 5)
 ohbot.move(ohbot.EYETILT, 5)
+
+
+ohbot.say("Putting on the ritz", False, True)
 # blinking
 ohbot.move(ohbot.LIDBLINK, 0)
 ohbot.wait(0.4)
 ohbot.move(ohbot.LIDBLINK, 10)
-time.sleep(1)  # Wait for 1 second
 
-ohbot.say("Putting on the ritz", False, True)
+ohbot.move(ohbot.HEADTURN, 5, spd=3)
+ohbot.move(ohbot.HEADNOD, 5, spd=3)
+ohbot.wait(2)  # Wait for 2 seconds
+
+
+ohbot.say("Dressed up like a million-dollar trouper", False, True)
+
+ohbot.wait(.5)
+ohbot.say("Trying hard to look like Gary Cooper", False, True)
+ohbot.say("Super duper", False, True)
+
+ohbot.wait(1) 
+ohbot.say("If you're blue, and you don't know where to go to", False, True)
+ohbot.wait(.5)  # Wait for 2 seconds
+ohbot.say("Why don't you go where fashion sits?", False, True)
+ohbot.say("Puttin' on the Ritz", False, True)
+
+ohbot.wait(2)  # Wait for 2 seconds
+ohbot.say("Puttin' on the Ritz", False, True)
+ohbot.say("Puttin' on the Ritz", False, True)
+ohbot.say("Puttin' on the Riiiitzzzz", False, True)
 
 # close ohbot at the end.
 ohbot.reset()
